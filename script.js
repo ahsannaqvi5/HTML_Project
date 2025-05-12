c50fabd3c413aa4c398cec497bec872bc545e159
ahsannaqvi5 <ahsan.raza@mnsrobotics.com>
2025-05-12 22:22:04 +0500

    .

---
+++ script.js
@@ -0,0 +1,20 @@
+let xp =0;
+let health = 100;
+let gold = 50;
+let currentweapon = 0;
+let fighting;
+let monterHealth;
+let inventory = ["stick"];
+
+const button1 = document.querySelector("button1");
+const button2 = document.querySelector("button2");
+const button3 = document.querySelector("button3");
+
+ // initialize buttons
+ button1.onclick =goStore;
+ button1.onclick =goCave;
+ button1.onclick =fightdrag;
+ 
+ function update(location) {
+
+ }
