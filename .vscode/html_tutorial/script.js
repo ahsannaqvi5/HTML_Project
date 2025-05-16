b8296693a8707525aef2f250d6732f4fb4704263
ahsannaqvi5 <ahsan.raza@mnsrobotics.com>
2025-05-16 08:08:40 +0500

    some new tags learn

---
+++ .vscode/html_tutorial/script.js
@@ -0,0 +1,7 @@
+const APILINK = ''
+const IMG_PATH = ''
+const SEARCHAPI = ''
+
+const main = document.getElementById("section");
+const form = document.getElementById("form");
+const search = document.getElementById("query");
